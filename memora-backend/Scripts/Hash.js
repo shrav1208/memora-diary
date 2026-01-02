f1256635f5398223b37b007837dd3e59560833f4
Aditya Sontakke <adityasontakke88@gmail.com>
2026-01-02 16:39:54 +0530

    installed bcrypt and created hashing script to hash unhashed passwords

---
+++ memora-backend/Scripts/Hash.js
@@ -0,0 +1,29 @@
+import bcrypt from "bcrypt"
+import User from "../Models/User.js"
+import mongoose from "mongoose";
+
+(async ()=>{
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/test")
+
+        console.log("MongoDB connected succeessfully")
+
+        const users = await User.find();
+
+        for(let user of users){
+            if (!user.password || typeof user.password !== "string") continue;
+            if (user.password.startsWith("$2")) continue;
+            const hashPassword = await bcrypt.hash(user.password, 10);
+            user.password = hashPassword;
+            await user.save();
+        }
+
+        console.log("Passwords hashed") 
+
+        await mongoose.disconnect();
+        process.exit(0);    
+    }catch(err){
+    console.error(err);
+    process.exit(1);
+    }
+})();
