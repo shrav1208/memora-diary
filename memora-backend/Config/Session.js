d940219f94b7df178861d914d32a61f221d03380
Aditya Sontakke <adityasontakke88@gmail.com>
2026-01-04 20:02:51 +0530

    imported express-session and connect-mongo for session based auth, created config folder with empty db file and session.js for session auth and imported it to server file, created env file (git ignored) and used in server and session files, also moved around things in the sever file for readability and order of execution

---
+++ memora-backend/Config/Session.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+export default session({
+    name: "mem-session-id", //cookie name
+
+    secret: process.env.SESSION_SECRET, //signing key
+
+    resave: false, //re save session on every request (false)
+    saveUninitialized: false, //create sessions for anonymous users who are not logged in (false)
+
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URL,
+        collectionName: "sessions",
+        ttl: 60*60*24, //seconds (1 day expiry)
+    }),
+
+    cookie: {
+        httpOnly: true,
+        secure: false, //false for localhost change to true for prod
+        sameSite: "lax", // CSRF protection
+        maxAge: 1000*60*60*24, // milliseconds (1 day expiry)
+    }
+    
+
+})
--- memora-backend/Server.js
+++ memora-backend/Server.js
@@ -1,24 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.PORT, process.env.SESSION_SECRET, process.env.MONGO_URL);
 
 import loginRoutes from './Routes/Login.routes.js';
 import signupRoutes from './Routes/Signup.routes.js';
+import sessionConfig from './Config/Session.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-const mongoURL = "mongodb://127.0.0.1:27017/test";
+app.use(sessionConfig);
+
+app.use('/api/login', loginRoutes);
+app.use('/api/signup', signupRoutes);
+
+const mongoURL = process.env.MONGO_URL;
 
 mongoose.connect(mongoURL)
 .then(()=>console.log("MongoDB connected succeessfully"))
 .catch((err=>console.log("MongoDB connection error ", err)));
-
-app.use('/api/login', loginRoutes);
-app.use('/api/signup', signupRoutes);
 
 app.get('/api/test', (req, res)=>{
     res.send('success');
