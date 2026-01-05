ebdcb9591f8e138fe98624a476fa5f47ef08827f
Aditya Sontakke <adityasontakke88@gmail.com>
2026-01-06 01:13:18 +0530

    fixed the middleware from req.session.userId to req.session.userID

--- memora-backend/Middleware/Auth.js
+++ memora-backend/Middleware/Auth.js
@@ -1,5 +1,5 @@
 export default function requireAuth(req, res, next){
-    if(!req.session.userId){
+    if(!req.session.userID){
         return res.status(401).json({ 
             success: false, 
             message: 'Unauthorized',
