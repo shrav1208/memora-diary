957170afb4572703930280fec796672d0379bea5
Aditya Sontakke <adityasontakke88@gmail.com>
2026-01-08 18:55:05 +0530

    forgot to declare router as a const in search post route lol

--- memora-backend/src/Routes/SearchPosts.routes.js
+++ memora-backend/src/Routes/SearchPosts.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { searchPosts } from "../Controllers/SearchPosts.controllers.js";
 
-router = express.Router();
+const router = express.Router();
 
 router.get('/', searchPosts);
 
