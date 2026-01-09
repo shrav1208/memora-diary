2be5a456fd5493f675966b133db3e17d85c6e475
Shravani Deshpande <deshravani@gmail.com>
2026-01-09 13:12:37 +0530

    created the all years display page (component), years data fetching backend endpoints and controllers, updated routes

---
+++ memora-frontend/src/components/All/All.jsx
@@ -0,0 +1,51 @@
+
+import styles from './All.module.css'
+import { Link, useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { YearCard } from './YearCard';
+
+export const All = () => {
+
+    const [years, setYears] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const loadYears = async () => {
+            try {
+                const res = await axios.get("/api/get/years", {
+                    withCredentials: true
+                });
+                console.log(res.data);
+                
+                if (res.data.success) {
+                    setYears(res.data.years);
+                }
+            } catch (err) {
+                console.error("Failed to fetch diary entries:", err);
+            }
+
+        };
+
+        loadYears();
+    }, []);
+
+
+
+
+    return (
+        <>
+            <div className={styles['years-collection']}>
+                {years.map((y) => (
+                    <YearCard
+                        key={y.year}
+                        year={y.year}
+                        count={y.count}
+                        onClick={() => navigate(`/dashboard/year`)}
+                    // add /${y.year} later
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
