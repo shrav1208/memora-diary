efb6a3c5c3374ef551949a72a254195370f31780
Shravani Deshpande <deshravani@gmail.com>
2026-01-10 21:36:30 +0530

    fixed days in month rendering logic (fill empty spaces in week before first day), gave special styling (highlight) to today's day component

--- memora-frontend/src/components/Month/DayCard.jsx
+++ memora-frontend/src/components/Month/DayCard.jsx
@@ -1,12 +1,28 @@
+import dayjs from 'dayjs';
 import styles from './DayCard.module.css'
 
-export const DayCard = ({ day, count, onClick }) => {
+export const DayCard = ({ day, month, year, count, onClick }) => {
+
+    const isToday = dayjs().isSame(
+        dayjs().year(year).month(month).date(day),
+        'day'
+    );
+
+    const cardClass = [
+        styles[count === 0 ? 'day-card' : 'day-card-exist'],
+        isToday && styles['today']
+    ].filter(Boolean).join(' ');
+
+    const dayClass = [
+        styles[count === 0 ? 'day-heading' : 'day-heading-exist'],
+        isToday && styles['today-heading']
+    ].filter(Boolean).join(' ');
+    
+
+
     return (
-        <div 
-            className={styles[`${count === 0 ? 'day-card' : 'day-card-exist'}`]}
-            onClick={onClick}
-        >
-            <p className={styles[`${count === 0 ? 'day-heading' : 'day-heading-exist'}`]}>{day}</p>
+        <div className={cardClass} onClick={onClick}>
+            <p className={dayClass}>{day}</p>
             <p className={styles[`${count === 0 ? 'count' : 'count-exist'}`]}>{count}</p>
         </div>
     );
