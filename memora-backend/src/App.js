f6e3f914f91a8f25fb8295b284283feac945307f
Aditya Sontakke <adityasontakke88@gmail.com>
2026-01-15 21:25:52 +0530

    created basic set mood endpoint but functionality isnt yet completely defined, will do when design of the feature is confirmed

--- memora-backend/src/App.js
+++ memora-backend/src/App.js
@@ -17,6 +17,7 @@ import getMonthsRoutes from './Routes/GetMonths.routes.js';
 import getDaysRoutes from './Routes/GetDays.routes.js';
 import sessionTimezone from './Routes/SessionTimezone.routes.js';
 import getMoods from './Routes/GetMoods.routes.js';
+import setMood from './Routes/SetMood.routes.js';
 
 import sessionConfig from './Config/Session.js';
 
@@ -68,6 +69,7 @@ app.use('/api/session/timezone', requireAuth, sessionTimezone);
 //Mood
 
 app.use('/api/get/moods', requireAuth, getMoods);
+app.use('/api/set/mood', requireAuth, setMood);
 
 //Test
 
