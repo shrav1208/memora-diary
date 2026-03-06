f7b6db69526b21c3dbb9c150957063cea29bb668
Shravani Deshpande <deshravani@gmail.com>
2026-03-06 13:49:07 +0530

    Improved the responsiveness of mainly DashboardHeader components

--- memora-frontend/src/pages/Dashboard/QuoteCard.jsx
+++ memora-frontend/src/pages/Dashboard/QuoteCard.jsx
@@ -23,10 +23,12 @@ export const QuoteCard = () => {
             <p className={styles['quote-heading']}>
                 {reflection?.heading || "Keep Moving Forward"}
             </p>
-            <p className={styles['quote']}>
-                {reflection?.body ||
-                    "Life is rarely a straight road; there will be obstacles, delays, and unexpected turns. What matters is that you keep moving, even if your steps are small. Progress is progress, and every little effort compounds over time into something meaningful."}
-            </p>
+            <div className={styles['quote-outer']}>
+                <p className={styles['quote']}>
+                    {reflection?.body ||
+                        "Life is rarely a straight road; there will be obstacles, delays, and unexpected turns. What matters is that you keep moving, even if your steps are small. Progress is progress, and every little effort compounds over time into something meaningful."}
+                </p>
+            </div>
         </div>
     );
 };
